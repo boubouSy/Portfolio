b0867f447bda9c814dd82d656a951d28c4256663
boubouSy <bouboujaddel19@gmail.com>
2026-02-19 18:45:14 +0100

    meportfolio2.py

--- meportfolio2.py
+++ meportfolio2.py
@@ -3,7 +3,7 @@ import streamlit as st
 #Configuration de la page
 st.set_page_config(
     page_title = "Boubou SY - Portfolio",
-    layout = "centered"
+    layout = "wide"
 )
 
 #Ma partie sidebar
